a83e49eb3016c3f87b36845eaa47a8c4e9655622
Sean Mahan <smahan@gmail.com>
2013-11-04 08:40:44 -0500

    Checking in the ScrumStandingsFetcher and the test that just skips getting the data for the Team w/no display name.

--- src/net/rugby/foundation/admin/server/model/ScrumStandingsFetcher.java
+++ src/net/rugby/foundation/admin/server/model/ScrumStandingsFetcher.java
@@ -1,6 +1,7 @@
 package net.rugby.foundation.admin.server.model;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,81 +16,152 @@ import net.rugby.foundation.model.shared.ITeamGroup;
 
 public class ScrumStandingsFetcher implements IStandingsFetcher {
 
-	private IRound r = null;
-	private ICompetition c = null;
-	private IUrlCacher uc = null;
-	private IStandingFactory sf;
-	private String url = null;
-	
-	@Inject
-	ScrumStandingsFetcher(IStandingFactory sf) {
-		this.sf = sf;
-	}
-	
-	
-	@Override
-	public void setRound(IRound r) {
-		this.r = r;
+    private IRound round = null;
+    private ICompetition comp = null;
+    private IUrlCacher urlCacher = null;
+    private IStandingFactory standingFetcher = null;
+    private List<String> standingTablesList = null;
+    private String url = null;
 
-	}
+    @Inject
+    ScrumStandingsFetcher(IStandingFactory sf) {
+        this.standingFetcher = sf;
+    }
 
-	@Override
-	public void setComp(ICompetition c) {
-		this.c = c;
+    @Override
+    public void setRound(IRound r) {
+        this.round = r;
+    }
 
-	}
+    @Override
+    public void setComp(ICompetition c) {
+        this.comp = c;
+    }
 
-	@Override
-	public IStanding getStandingForTeam(ITeamGroup t) {
+    @Override
+    public IStanding getStandingForTeam(ITeamGroup team) {
 
-		assert (c != null);
-		assert (r != null);
-		assert (url != null);
-		assert (uc != null);
-		
-		try {
-		uc.setUrl(url);
-		Iterator<String> it = uc.get().iterator();
-		
-		// do some stuff and find the standings from the ?template=pointstable file
-		// testData is http://www.espnscrum.com/heineken-cup-2013-14/rugby/series/191757.html?template=pointstable on 10/13/13
-		
-		
+        assert (comp != null);
+        assert (round != null);
+        assert (url != null);
+        assert (urlCacher != null);
 
-		
-		IStanding s = sf.create();
-		s.setRound(r);
-		s.setRoundId(r.getId());
-		s.setTeam(t);
-		s.setTeamId(t.getId());
-		
-		s.setStanding(9999); // << There's the thing you have to fill in
-		
-		return s;
-		} catch (Throwable ex) {
-			Logger.getLogger(this.getClass().getCanonicalName()).log(Level.SEVERE,ex.getMessage(),ex);
-			return null;
-		}
-	}
+        try {
+            urlCacher.setUrl(url);
 
+            if (standingTablesList == null) {
+                processStandingTablesList();
+            }
 
-	public IUrlCacher getUc() {
-		return uc;
-	}
+            IStanding standing = standingFetcher.create();
+            standing.setRound(round);
+            standing.setRoundId(round.getId());
 
-	@Override
-	public void setUc(IUrlCacher uc) {
-		this.uc = uc;
-	}
+            standing.setTeam(team);
+            standing.setTeamId(team.getId());
 
+            searchTeamStanding(standing);
+            return standing;
 
-	public String getUrl() {
-		return url;
-	}
+        } catch (Throwable ex) {
+            Logger.getLogger(this.getClass().getCanonicalName()).log(Level.SEVERE, ex.getMessage(), ex);
+            return null;
+        }
+    }
 
-	@Override
-	public void setUrl(String url) {
-		this.url = url;
-	}
+    private void processStandingTablesList() {
+
+        assert (urlCacher != null);
+
+        if (standingTablesList == null) {
+            standingTablesList = new ArrayList<String>();
+        }
+
+        List<String> wholeFile = urlCacher.get();
+
+        int poolTableStart = 0;
+        int poolTableEnd = 0;
+        for (int i = 0; i < wholeFile.size(); i++) {
+            String line = wholeFile.get(i);
+            if (line.contains("<div id=\"scrumArticlesBoxContent\">")) {
+                poolTableStart = i + 1;
+            }
+            if (line.contains("<div class=\"liveCntSep\"></div>")) {
+                poolTableEnd = i;
+            }
+        }
+        while (poolTableStart < poolTableEnd) {
+            String addLine = wholeFile.get(poolTableStart);
+            if (addLine.contains("<p ")) {
+                poolTableStart = poolTableEnd;
+                break;
+            } else {
+                standingTablesList.add(addLine);
+                poolTableStart++;
+            }
+        }
+//        writeListToFile(standingTablesList, "StandingsTables5");
+    }
+
+    private void searchTeamStanding(IStanding standing) {
+
+        for (int i = 0; i < standingTablesList.size(); i++) {
+            String curLine = standingTablesList.get(i);
+            ITeamGroup team = standing.getTeam();
+            if (team != null) {
+                String teamName = standing.getTeam().getDisplayName();
+                if (!teamName.equals(null)) {
+                    if (curLine.contains(teamName)) {
+                        String standingLine = standingTablesList.get(i - 1);
+                        if (!standingLine.equals(null)) {
+                            try {
+                                int standingVal = Integer.parseInt(standingLine.split("<|/|>")[2]);
+                                standing.setStanding(standingVal);
+                                break;
+                            } catch (Throwable ex) {
+                                Logger.getLogger(this.getClass().getCanonicalName()).log(Level.SEVERE, ex.getMessage(), ex);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+//    private void writeListToFile(List<String> subLines, String filename) {
+//        if (subLines != null && subLines.size() > 0) {
+//            Iterator<String> iter = subLines.iterator();
+//            PrintWriter writer;
+//            try {
+//                writer = new PrintWriter("C:\\users\\seanm\\Development\\JAVA_SRC\\TEMP_FILES\\" + filename + ".html", "UTF-8");
+//                while (iter.hasNext()) {
+//                    writer.println(iter.next());
+//                }
+//                writer.close();
+//            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+//            } catch (UnsupportedEncodingException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
+    public IUrlCacher getUc() {
+        return urlCacher;
+    }
+
+    @Override
+    public void setUc(IUrlCacher uc) {
+        this.urlCacher = uc;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
 }
